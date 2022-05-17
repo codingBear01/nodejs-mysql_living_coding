4b5b0b754c97f76c56bfcfd75068be5f75573cd8
codingBear <95178119+GDK01@users.noreply.github.com>
2022-05-17 18:35:09 +0900

    fin

--- main.js
+++ main.js
@@ -1,151 +1,41 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const template = require('./lib/template.js');
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
+import http from 'http';
+import url from 'url';
+import * as handlePage from './lib/handlePage.js';
+import * as handleAuthor from './lib/handleAuthor.js';
 
 const app = http.createServer(function (request, response) {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
+
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      fs.readdir('./data', function (error, filelist) {
-        const title = 'Welcome';
-        const description = 'Hello, Node.js';
-        const list = template.list(filelist);
-        const html = template.HTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
+      handlePage.index(request, response);
     } else {
-      fs.readdir('./data', function (error, filelist) {
-        const filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-          const title = queryData.id;
-          const sanitizedTitle = sanitizeHtml(title);
-          const sanitizedDescription = sanitizeHtml(description, {
-            allowedTags: ['h1'],
-          });
-          const list = template.list(filelist);
-          const html = template.HTML(
-            sanitizedTitle,
-            list,
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
-                  <input type="submit" value="delete">
-                </form>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-      });
+      handlePage.renderOnePage(request, response);
     }
   } else if (pathname === '/create') {
-    fs.readdir('./data', function (error, filelist) {
-      const title = 'WEB - create';
-      const list = template.list(filelist);
-      const html = template.HTML(
-        title,
-        list,
-        `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-        ''
-      );
-      response.writeHead(200);
-      response.end(html);
-    });
+    handlePage.createPage(request, response);
   } else if (pathname === '/create_process') {
-    const body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      const post = qs.parse(body);
-      const title = post.title;
-      const description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.writeHead(302, { Location: `/?id=${title}` });
-        response.end();
-      });
-    });
+    handlePage.createPageProcess(request, response);
   } else if (pathname === '/update') {
-    fs.readdir('./data', function (error, filelist) {
-      const filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        const title = queryData.id;
-        const list = template.list(filelist);
-        const html = template.HTML(
-          title,
-          list,
-          `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    });
+    handlePage.updatePage(request, response);
   } else if (pathname === '/update_process') {
-    const body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      const post = qs.parse(body);
-      const id = post.id;
-      const title = post.title;
-      const description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
-          response.end();
-        });
-      });
-    });
+    handlePage.updatePageProcess(request, response);
   } else if (pathname === '/delete_process') {
-    const body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      const post = qs.parse(body);
-      const id = post.id;
-      const filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function (error) {
-        response.writeHead(302, { Location: `/` });
-        response.end();
-      });
-    });
+    handlePage.deletePage(request, response);
+  } else if (pathname === '/author') {
+    handleAuthor.authorTable(request, response);
+  } else if (pathname === '/create_author') {
+    handleAuthor.createAuthor(request, response);
+  } else if (pathname === '/create_author_process') {
+    handleAuthor.createAuthorProcess(request, response);
+  } else if (pathname === '/update_author') {
+    handleAuthor.updateAuthor(request, response);
+  } else if (pathname === '/update_author_process') {
+    handleAuthor.updateAuthorProcess(request, response);
+  } else if (pathname === '/delete_author') {
+    handleAuthor.deleteAuthor(request, response);
   } else {
     response.writeHead(404);
     response.end('Not found');
